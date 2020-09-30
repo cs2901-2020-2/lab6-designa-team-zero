029e69fa872d176079e3999aea908761f6c4ac10
root <root@DESKTOP-OO1V4NK.localdomain>
2020-09-30 11:10:17 -0500

    fixing methods errors

--- src/main/java/entities/EstacionMeteorologica.java
+++ src/main/java/entities/EstacionMeteorologica.java
@@ -2,35 +2,35 @@ package entities;
 import java.util.List;
 import java.util.ArrayList;
 import java.security.SecureRandom;
-import Cliente;
 
-public class  EstacionMetereologica {
-    List<vector> clients = new ArrayList<>();
+public class  EstacionMeteorologica {
+    List<Cliente> clients = new ArrayList<>();
     String Name;
-    int size = 0, temp{} , humidity{}, wind{};
+    int size, temp , humidity, wind;
 
     public void ActualizarDatos(){
         SecureRandom random = new SecureRandom();
-        this->temp = random.nextInt(40);
-        this->humidity = random.nextInt(100);
-        this->wind = random.nextInt(20);
+        this.temp = random.nextInt(40);
+        this.humidity = random.nextInt(100);
+        this.wind = random.nextInt(20);
 
-        for(Cliente client: clientes){
+        for(Cliente client: clients){
             client.update(temp,wind,humidity);
         }
     }
 
-    public EstacionMetereologica(){}
+    public EstacionMeteorologica(){}
 
-    public EstacionMetereologica(String name){
+    public EstacionMeteorologica(String name){
         Name = name;
     }
 
-    public SizeOfClients(List<Cliente> clients){
+    public int SizeOfClients(List<Cliente> clients){
         size = clients.size();
+        return size;
     }
 
-    public NuevoCliente(Cliente client){
+    public void NuevoCliente(Cliente client){
         clients.add(client);
         size++;
     }
